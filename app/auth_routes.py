a53fa3a6860d9f9e86fdc8ae029e9d35ff27054d
Hannah Schultz <hmschult1@geneva.edu>
2026-07-20 11:27:36 -0400

    Update login UI

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -37,12 +37,12 @@ def login():
 
         flash('Invalid email or password.', 'danger')
 
-    return render_template('auth/login.html', form=form)
+    return render_template('admin_panel/login.html', form=form)
 
 @auth_bp.route('/logout')
 @login_required
 def logout():
     logout_user()
     session.clear()
-    flash('You have been logged out. Thank you for using the Geneva Web Portal!', 'info')
+    flash('You have been logged out. Thank you for using the Geneva App Portal!', 'info')
     return redirect(url_for('auth.login'))
